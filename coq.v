f9d54f8b379393a81eb0f78be46ceab37da5c3ee
Kelley van Evert <kelley.vanevert@gmail.com>
2011-06-16 19:31:11 +0200

    Drastic renaming, removed rotation, general improved consistency and quality.

--- coq.v
+++ coq.v
@@ -13,40 +13,14 @@ Axiom Natuurlijk: (forall x:T, x >= 0) /\ (forall x:G, x >= 0)
 (* Of er (genoeg, etc) stroom naar de elevator gaat *)
 Variable stroom: B.
 
-(* Rotaties per seconde van de as die de molenstenen aandraait,
-   en de schuddebak van energie voorziet *)
-Variable rotatie: Z.
-Axiom Natuurlijk2: rotatie >= 0.
-
 Variable TODO: B.
 
-(*
-    Predikaten GA, GB, GC
-    GA t g: Er is {g} gram graan op tijdstip {t} aanwezig op plek A
-    - A: onderaan de lift
-    - B: bovenaan de lift
-    - C: invoer molenstenen
-*)
-Variables GA GB GC: T -> G -> B.
-
-(*
-    Predikaten MD, ME
-    MD t m: Er is {m} gram meel op tijdstip {t} aanwezig op plek D
-    - D: bovenaan de meelpijp
-    - E: invoer zak
-    - Z: in de zak -- deze wordt gebruikt in Molenaar
-*)
-Variables MD ME MZ: T -> M -> B.
-
-(*
-    Predikaat ZF
-    ZF t z: Op tijdstip {t} zijn er {z} zakken volledig gevuld
-*)
-Variable  ZF:       T -> Z -> B.
+Variables graan kaap geschud:    T -> G -> B.
+Variables gemalen naarzak inzak: T -> M -> B.
+Variable  zakken:                T -> Z -> B.
 
 (* Constanten > Algemeen *)
 Definition maxSec := 60 * 60 * 24. (* Aantal seconden in een dag *)
-Definition rMin := 20. (* Minimale rotatie *)
 
 (* Constanten > Elevator *)
 Definition eD:T  := 3%Z.   (* tijd tot volgende bak *)
@@ -61,24 +35,23 @@ Definition pTD:T  := 5.   (* De tijdsduur die het duurt tot meel van de ingang n
 Definition zMin:M := 15000. (* Minimale meel in zak (inclusief) *)
 Definition zMax:M := 25000. (* Maximale meel in zak (exclusief) *)
 
-(*
-    IN:  stroom, GA
-    UIT: GB
-*)
 Definition Elevator :=
   stroom -> (
-    forall t:T, (exists k:Z, k >= 0 /\ t = k * eD) -> (
+    forall t:T, (exists k:Z, k >= 0 /\ t = k * eD) ->
+    (
         (* Elke {eD} sec wordt {eG} gram graan meegenomen door de elevator *)
-        (forall g:G, g%Z >= eG -> GA t g -> GA (t + eD) (g - eG))
-     /\ (forall g:G, g >= 0  -> GA t g -> GA (t + eD) 0)
+        (forall g:G, (g > eG  -> graan t g -> graan (t + eD) (g - eG))
+                  /\ (g <= eG -> graan t g -> graan (t + eD) 0)
+        )
+        
         (* Elke {eTD} sec later arriveert deze bovenaan *)
-     /\ (forall g:G, g >= eG -> (forall gb:G,
-            GA t g /\ GB (t + eTD-eD) gb ->  GB (t + eTD) (gb + eG)))
-     /\ (forall g:G, g >= 0  -> (forall gb:G,
-            GA t g /\ GB (t + eTD-eD) gb ->  GB (t + eTD) (gb + g)))
+     /\ (forall g:G, g > eG  -> (forall gk:G, graan t g /\ kaap (t + eTD - eD) gk -> kaap (t + eTD) (gk + eG))
+                  /\ g <= eG -> (forall gk:G, graan t g /\ kaap (t + eTD - eD) gk -> kaap (t + eTD) (gk + g))
+        )
+        
         (* Alle tijdstippen tussen n*eD en (n+1)*eD *)
      /\ (forall g:G, forall s:T, (s >= t /\ s < t+eD) ->
-            (GA s g <-> GA t g) /\ (GB s g <-> GB t g))
+            (graan s g <-> graan t g) /\ (kaap s g <-> kaap t g))
   ))
 .
 
@@ -94,36 +67,28 @@ Definition Schuddebak := TODO.
 *)
 Definition Molenstenen := TODO.
 
-(*
-    IN:  MD
-    UIT: ME
-*)
 Definition Meelpijp :=
-    (MD 0 0) /\
-    (ME 0 0) /\
+    (gemalen 0 0) /\
+    (naarzak 0 0) /\
     (forall t:T, forall m:M,
-        (m > pCap -> MD t m -> ME (t + pTD) pCap)
-     /\ (m < pCap -> MD t m -> ME (t + pTD) m)
+        (m > pCap -> gemalen t m -> naarzak (t + pTD) pCap)
+     /\ (m < pCap -> gemalen t m -> naarzak (t + pTD) m)
     ).
 
-(*
-    IN:  ME
-    UIT: ZF
-*)
 Definition Molenaar :=
     (* Begintoestand: nog geen meel in zak, nog geen gevulde zakken *)
-    (MZ 0 0) /\
-    (ZF 0 0) /\
+    (inzak 0 0) /\
+    (zakken 0 0) /\
     (* De zak mag nooit overstromen *)
-    ~(exists t:T, exists m:M, MZ t m /\ m >= zMax) /\
+    ~(exists t:T, exists m:M, inzak t m /\ m >= zMax) /\
     (* Verder geldt dat.. *)
     (forall t:T, forall mprev:M, forall madd:M, forall zprev:ZAKKEN,
-        (MZ t mprev /\ ME t madd /\ ZF t zprev)
+        (inzak t mprev /\ naarzak t madd /\ zakken t zprev)
      -> (
             (* Zak wordt enkel bijgevuld door toevoer stroom meel *)
-            (                 MZ (t + 1) (mprev + madd) /\ ZF (t + 1) zprev) \/
+            (                 inzak (t + 1) (mprev + madd) /\ zakken (t + 1) zprev) \/
             (* OF, indien al meer dan {zMin} meel in zak, nieuwe zak! *)
-            (mprev >= zMin /\ MZ (t + 1) madd           /\ ZF (t + 1) (zprev + 1))
+            (mprev >= zMin /\ inzak (t + 1) madd           /\ zakken (t + 1) (zprev + 1))
         )
     ).
 
